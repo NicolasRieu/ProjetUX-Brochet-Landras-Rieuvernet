ac6076d44ca8edaecca3173d493b69e0b0c4a3d1
NicoRieu <rieuvernetnicolas@gmail.com>
2025-10-06 09:46:44 +0200

    Refactor population data processing to filter out 'World' entity and ensure valid country codes

--- scripts/filterCountriesPopulation.py
+++ scripts/filterCountriesPopulation.py
@@ -16,6 +16,12 @@ if 'Population (projections)' in df.columns:
 # Filtrer les années entre 1900 et 2016 inclus et garder uniquement les lignes avec un Code non null
 df_filtered = df[(df['Year'] >= 1900) & (df['Year'] <= 2016) & (df['Code'].notna())]
 
+# Supprimer les entitées sans code pays
+df_filtered = df_filtered[df_filtered['Code'].notna()]
+
+# Supprimer l'entité "World"
+df_filtered = df_filtered[df_filtered['Entity'] != 'World']
+
 
 # Sauvegarder le résultat dans un nouveau fichier
 output_path = os.path.join(project_root, 'CSV_Dest', 'population-1900-2016.csv')
