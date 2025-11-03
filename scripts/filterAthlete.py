382c674dda44333b7b847f9a173b00a39d2d3913
NicoRieu <rieuvernetnicolas@gmail.com>
2025-11-03 11:39:21 +0100

    filterAthlete.py modified to exclude athlete data before 1900 (was causing problems with graphs)

--- scripts/filterAthlete.py
+++ scripts/filterAthlete.py
@@ -13,6 +13,9 @@ df = pd.read_csv(input_path,sep=";")
 # Supprimer les colonnes inutiles
 df = df.drop(columns=["Height", "Weight", "City","Age"])
 
+# Filtrer les données pour ne garder que les années >= 1900 (cohérence avec les données de population)
+df = df[df["Year"] >= 1900]
+
 # Sauvegarder le nouveau CSV
 output_path = os.path.join(project_root, 'CSV_Dest', 'athlete_events_opti.csv')
 os.makedirs(os.path.dirname(output_path), exist_ok=True)
